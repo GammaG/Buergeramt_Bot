da888e95479fb5b4f0e3ba231a6ac74c6f8e862e
mseidler <marco.seidler@outlook.com>
2021-12-28 12:44:24 +0100

    change behaviour for chrome

--- buergeramt_bot.py
+++ buergeramt_bot.py
@@ -3,16 +3,21 @@ import webbrowser
 import bs4
 import requests
 
-#Dienstleistung Einkommentieren:
+#Dienstleistung Einkommentieren für manuelles festlegen:
 #KFZ Abmeldung
-dienstleistung = 120877
+#dienstleistung = 120877
 
+#Personalausweiß erneuern
+#dienstleistung = 120703
+
+#Führerschein umtauschen
+dienstleistung = 124556
 
 
 url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&anliegen%5B%5D='+str(dienstleistung)+'&herkunft=%2Fterminvereinbarung%2F'
 
-browser_path="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-webbrowser.register('firefox', None,webbrowser.BackgroundBrowser(browser_path))
+browser_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(browser_path))
 
 while True:
     response = requests.get(url)
@@ -27,11 +32,10 @@ while True:
         for link in links[:10]:
             if 'href' in link.attrs:
                 day = int(link.text.strip())
-                if day > 15 and day < 20:
-                    found = True
-                    link = ('https://service.berlin.de{} '.format(link.attrs['href']))
-                    print(link)
-                    webbrowser.get('firefox').open_new_tab(link)
+                found = True
+                link = ('https://service.berlin.de{} '.format(link.attrs['href']))
+                print(link)
+                webbrowser.get('chrome').open_new_tab(link)
         if found:
             break
     time.sleep(20)
